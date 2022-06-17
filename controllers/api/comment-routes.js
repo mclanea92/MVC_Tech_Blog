66d085c2e561588f9b7fb070fc7233ec0d7cfd86
Adam McLane <mclanea92@gmail.com>
2022-06-17 15:39:44 -0400

    updated api routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,3 +1,39 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../..utils/auth');
+
+router.get('/', (req, res) => {
+    Comment.findAll({})
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+});
+
+router.get('/:id', (req, res) => {
+    Comment.findAll({
+        where: { id: req.params.id }
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        res.status(500).json(err)
+        console.log(err)
+    })
+});
+
+router.post('/', withAuth, (req, res) => {
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            res.status(500).json(err)
+            console.log(err)
+        })
+    }
+});
+
