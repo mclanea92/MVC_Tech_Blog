6c1f97b45e92b212b624c9c007ec67080f821ec8
Adam McLane <mclanea92@gmail.com>
2022-06-17 14:41:13 -0400

    added server

---
+++ server.js
@@ -0,0 +1,40 @@
+const routes = require('./controllers');
+const express = require('express');
+const sequelize = require('./config/connection');
+const path = require('path');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: { maxAge: 36000 },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(routes);
+
+app.engine('handlebars', hbs.engine);
+
+app.use(session(sess));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+
+app.set('view engine', 'handlebars');
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
