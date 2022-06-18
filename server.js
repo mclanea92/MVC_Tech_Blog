b179de5a7ab2e35c74df416f860faa270c13250c
Adam McLane <mclanea92@gmail.com>
2022-06-18 10:03:33 -0400

    fixed server

--- server.js
+++ server.js
@@ -36,5 +36,5 @@ app.use(express.json());
 app.set('view engine', 'handlebars');
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log(`Now listening at ${PORT}`));
 });
