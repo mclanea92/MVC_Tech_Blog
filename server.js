453e404e6414fd144a28d0e38bce5f367e7331ab
Adam McLane <mclanea92@gmail.com>
2022-06-19 12:24:51 -0400

    moved code around

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const routes = require('./controllers');
 const express = require('express');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 const sess = {
     secret: 'Super secret secret',
@@ -21,19 +23,20 @@ const sess = {
     })
 };
 app.use(session(sess));
-app.use(routes);
 
-app.engine('handlebars', hbs.engine);
-
-
+app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.json());
+app.engine('handlebars', hbs.engine);
 
 app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening at ${PORT}`));
